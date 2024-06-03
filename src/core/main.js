85e3eef7ef71648f84fa3f92196fc8f5bb6c7503
todd gureckis <gureckis@mac.com>
2024-06-03 13:30:08 -0400

    moving App.vue in to core (#104)

--- src/core/main.js
+++ src/core/main.js
@@ -5,7 +5,7 @@ import '@formkit/themes/genesis'
 import VueGtag from 'vue-gtag' // google analytics
 // import with an @ symbol are resolved by vite to ./src directory
 
-import App from '@/App.vue' // import the main app component
+import App from '@/core/App.vue' // import the main app component
 import { router } from '@/core/router' // import the router
 import { pinia } from '@/core/createpinia'
 import '@/core/icons' // configure fontawesome
