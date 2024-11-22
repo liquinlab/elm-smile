1a0f129369c8746f458691c97fccf5e25c838284
todd gureckis <gureckis@mac.com>
2024-11-21 19:08:35 -0500

    feat: adds support for browsing the private data in dev tools
    Issue #71

--- src/core/composables/useAPI.js
+++ src/core/composables/useAPI.js
@@ -20,6 +20,8 @@ export default function useAPI() {
   const api = {
     config: smilestore.config,
     data: smilestore.data,
+    private: smilestore.private,
+    all_data: { private: smilestore.private, data: smilestore.data },
     local: smilestore.local,
     global: smilestore.global,
     dev: smilestore.dev,
