1163d4d0a9373eb169861401c04641ad5dac10f9
todd gureckis <gureckis@mac.com>
2025-05-26 23:01:06 -0400

    removing resetOnce

--- src/core/stepper/StepState.js
+++ src/core/stepper/StepState.js
@@ -44,8 +44,6 @@ export class StepState {
     this._parent = parent
     this._root = parent?._root || this
     this._data = {}
-
-    this._resetOnce = false
   }
 
   /**
@@ -361,14 +359,6 @@ export class StepState {
   reset() {
     this._currentIndex = 0
     this._states.forEach((state) => state.reset())
-  }
-
-  resetOnce() {
-    if (this._resetOnce) return false
-    this._resetOnce = true
-    this._currentIndex = 0
-    this._states.forEach((state) => state.reset())
-    return true
   }
 
   /**
