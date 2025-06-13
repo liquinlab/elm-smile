f22872a98a70166a3fa898247749de87a1921bfa
todd gureckis <gureckis@mac.com>
2025-06-13 15:55:23 -0400

    moving coverage to a command line option instead of defatul

--- vite.config.js
+++ vite.config.js
@@ -59,7 +59,7 @@ export default ({ mode }) => {
       environment: 'happy-dom',
       coverage: {
         all: true,
-        enabled: true,
+        enabled: false,
         src: path.resolve(__dirname, './src'),
         reporter: ['text', 'html'],
         provider: 'v8',
