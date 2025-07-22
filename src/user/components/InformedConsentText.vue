39f3fbb8dc33be6cf52a3bb8f27a7a5e607e4a68
emilyliquin <emilyliquin@gmail.com>
2025-07-22 15:47:16 -0400

    smile template lab setup

--- src/user/components/InformedConsentText.vue
+++ src/user/components/InformedConsentText.vue
@@ -1,45 +1,117 @@
 <template>
   <div class="p-3 pt-2 text-left">
-    <h1 class="text-3xl font-bold"><i-mdi-approve class="inline-block mr-2" />Informed Consent</h1>
+    <h1 class="text-3xl font-bold">
+      <i-mdi-approve class="inline-block mr-2" />CONSENT FORM FOR PARTICIPATION IN A RESEARCH STUDY
+    </h1>
     <hr class="mt-2 mb-3 h-px" />
     <p>
-      If you agree to be in this study, you will be asked to look at pictures of objects or pictures presented on a
-      computer display, watch videos, listen to sounds, or observe lights controlled by the computer and to make
-      judgments about them using a keyboard, mouse, or other input device attached to a computer.
+      I'm Emily Liquin, an assistant professor in the Psychology Department at the University of New Hampshire and I am
+      the lead researcher investigating “Learning How to Seek Information” (IRB-FY2025-37).
     </p>
-    <ul class="pl-8 pt-3 pb-3 list-square">
-      <li>
-        Participation in this study will take about 30 minutes to complete, and you will receive $4.50 for your
-        participation. As an additional incentive to try your best at the task, you may earn a cash bonus of up to
-        $10.00 based on your performance.
-      </li>
-      <li>When you complete the study, a thorough verbal and written explanation of it will be provided.</li>
-      <li>In addition, by agreeing to participate, you understand you must be 18 years or older to participate.</li>
-      <li>
-        There are no known risks associated with your participation in this research beyond those of everyday life.
-        Although you will receive no direct benefits, this research may help the investigator understand how people
-        learn new categories.
-      </li>
-      <li>
-        Confidentiality of your research records will be strictly maintained. We assign code numbers to each participant
-        so that data is never directly linked to individual identity, and we are interested in group results rather than
-        the responses of particular individuals. Any emails will never be shared with anyone nor linked to your data.
-        Data are kept in our laboratory and are only viewed by the investigators. These data files are kept on our
-        computer indefinitely.
-      </li>
-      <li>
-        Taking part in this study is voluntary. Not taking part or withdrawing after the study has begun will result in
-        no loss of services from NYU to which you are otherwise entitled. You have the right to skip or not answer any
-        questions you prefer not to answer.
-      </li>
-    </ul>
+    <br />
+    <h2 class="font-bold">What is the purpose of this form?</h2>
     <p>
-      If there is anything about the study or your participation that is unclear or that you do not understand, if you
-      have questions or wish to report a research-related problem, you may contact the principal investigator,
-      <a href="http://todd.gureckislab.org">Todd M. Gureckis</a> at 6 Washington Place (409), New York, NY, 10003, (212)
-      998-3794, <a href="mailto:todd.gureckis@nyu.edu">todd.gureckis@nyu.edu</a>. For questions about your rights as a
-      research participant, you may contact the University Committee on Activities Involving Human Subjects, New York
-      University, 665 Broadway, Suite 804, New York, NY 10012, at ask.humansubjects@nyu.edu or (212) 998-4808.
+      This consent form describes the research study and helps you to decide if you want to participate. It provides
+      important information about what you will be asked to do in the study, about the risks and benefits of
+      participating in the study, and about your rights as a research participant. You should:
+    </p>
+
+    <ul>
+      <li>
+        Read the information in this document carefully, and ask me or the research personnel any questions,
+        particularly if you do not understand something.
+      </li>
+      <li>
+        Not agree to participate until all your questions have been answered, or until you are sure that you want to.
+      </li>
+      <li>
+        Understand that your participation in this study involves you to complete a learning task. In this task, you
+        will make choices to gather information. This study will last about 45 minutes.
+      </li>
+      <li>Understand that the potential risks of participating in this study are minimal.</li>
+    </ul>
+    <br />
+    <h2 class="font-bold">What is the purpose of this study?</h2>
+    <p>
+      The purpose of this study is to better understand how people make decisions about seeking information. We expect
+      that approximately 2000 people will participate in this study. To participate, one must be at least 18 years old.
+    </p>
+    <br />
+    <h2 class="font-bold">What does your participation in this study involve?</h2>
+    <p>
+      For this study, you will complete a learning task. Depending on the study, this may involve reading a story,
+      solving a puzzle, or playing a game. During the task, you will make choices to gather information. You may also be
+      asked to provide your opinions about certain aspects of the learning task. Finally, you will complete a
+      demographic questionnaire. The entire study will take approximately 45 minutes. You must be at least 18 years old
+      to participate.
+    </p>
+    <p>
+      Some of the materials for this study may have been produced by a generative AI (genAI) tool. In the instructions
+      of the study, we will inform you whether you will be reading materials produced by genAI.
+    </p>
+    <p>
+      You should complete the study only once. You will receive compensation for only one set of responses if you are
+      eligible to participate in the study. For questions about compensation or eligibility, please contact the
+      researcher (information provided at the end of the form).
+    </p>
+    <br />
+    <h2 class="font-bold">What are the possible benefits of participating in this study?</h2>
+    <p>
+      There are no direct benefits to participating in this study. Indirect benefits may include gaining insight into
+      the research process and feeling satisfaction at contributing to research. By taking part in this study, you are
+      helping us advance our scientific understanding of how people learn and seek information.
+    </p>
+    <br />
+    <h2 class="is-size-5">Will you receive any compensation for participating in this study?</h2>
+    <p>
+      As compensation for participation in this research study or the alternative assignment, you will receive 1 credit
+      hour of research experience towards a psychology course requirement of your choosing. The alternate assignment for
+      this study involves reading a short research article related to the study and writing a brief response paper.
+    </p>
+    <br />
+    <h2 class="font-bold">Do you have to take part in this study?</h2>
+    <p>
+      Taking part in this study is completely voluntary. You may choose not to take part at all. If you agree to
+      participate, you may refuse to answer any question. If you decide not to participate, you will not be penalized or
+      lose any benefits for which you would otherwise qualify.
+    </p>
+    <br />
+    <h2 class="is-size-5">Can you withdraw from this study?</h2>
+    <p>
+      If you agree to participate in this study and you then change your mind, you may stop participating at any time.
+      Any data collected as part of your participation will remain part of the study records. If you decide to stop
+      participating at any time, you will not be penalized or lose any benefits for which you would otherwise qualify.
+      However, you will not receive class credit unless you complete the study.
+    </p>
+    <br />
+    <h2 class="font-bold">How will the confidentiality of your records be protected?</h2>
+    <p>
+      I plan to maintain the confidentiality of all data and records associated with your participation in this
+      research. Any communication via the internet poses minimal risk of a breach of confidentiality.
+    </p>
+    <p>
+      To help protect the confidentiality of your information, all of your data will be de-identified after this study
+      is completed: your SONA ID number and your IP address will be removed from the data and replaced with a random ID
+      number. We will retain a separate data file (which will never be shared) that links your SONA ID number to your
+      random ID number. Your name will never appear on any of the records associated with this study. Electronic data
+      will be stored in a secure cloud storage system backed by the University System of New Hampshire IT Team. Only Dr.
+      Emily Liquin and her lab will have access. De-identified data (which cannot be linked back to your name/identity)
+      may be used for future studies or shared with other researchers.
+    </p>
+    <p>
+      The results of this study may be used in reports, presentations, publications, and grant applications. Results
+      will be reported in aggregate or using pseudonyms if applicable. Your name and identity will never appear
+      alongside any results from this study.
+    </p>
+    <br />
+    <h2 class="font-bold">Whom to contact if you have any questions about this study</h2>
+    <p>
+      If you have any questions pertaining to the research you can contact Emily Liquin, emily.liquin@unh.edu to discuss
+      them.
+    </p>
+    <p>
+      If you have questions about your rights as a research subject you can contact Melissa McGee in UNH Research
+      Integrity Services, 603/862-2005 or melissa.mcgee@unh.edu to discuss them.
     </p>
   </div>
 </template>
